ad49ba99f46e148f062192fccf0fddf054fb9fd6
brenoalmeidaa07 <albeidabreno15@gmail.com>
2025-01-10 17:07:58 -0300

    change made

--- src/components/StyledButton/StyledButton.js
+++ src/components/StyledButton/StyledButton.js
@@ -1,12 +1,7 @@
 import { styled } from "@mui/material/styles"; // Certifique-se de que este é o caminho correto dependendo da versão do MUI
-import { ReactNode } from 'react';
+import React from 'react';
 
-interface StyledButtonProps {
-  children: ReactNode;
-  onClick: () => void;
-}
-
-const CustomButton: React.FC<StyledButtonProps> = ({ children, onClick }) => { 
+const CustomButton = ({ children, onClick }) => { 
     const StyledButton = styled("button")(({ theme }) => ({
         backgroundColor: 'transparent',
         border: `1px solid ${theme.palette.primary.contrastText}`,     
