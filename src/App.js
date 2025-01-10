1d939be5166e41a3cacd0f5683b667cd63e7b019
brenoalmeidaa07 <albeidabreno15@gmail.com>
2025-01-10 14:59:23 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-const App = ()=> { 
+import Home from "./pages/Home/Home";
+
+const App= ()=> { 
   return (
     <>
-    App
+    
+   <Home/>
     </>
-   );
+   ); 
 }
 
-export default App;
+export default App;   
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import NavBar from "../../components/NavBar/NavBar";
+import Hero from "./sections/Hero/Hero";
+
+
+
+const Home = ()=> { 
+    return (
+      <>
+        <Hero />
+        <NavBar />
+      </>
+     ); 
+  }
+  
+  export default Home;
